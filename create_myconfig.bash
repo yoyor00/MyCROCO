a7ade418e9a59b8aa36a08aa6419e46bd6d7a2c6
gcambon <gildas.cambon@ird.fr>
2016-09-26 15:14:30 +0200

    Update ....

--- create_myconfig.bash
+++ create_myconfig.bash
@@ -23,40 +23,68 @@ MY_CONFIG_NAME='BENGUELA_LR'
 # Create the directory
 if [ ! -d $MY_CONFIG_PATH ]; then 
     mkdir $MY_CONFIG_PATH
-    mkdir $MY_CONFIG_PATH'/'MY_CONFIG_NAME
+    if [ ! -d $MY_CONFIG_PATH'/'$MY_CONFIG_NAME ]; then
+	mkdir $MY_CONFIG_PATH'/'$MY_CONFIG_NAME
+	copy_tag=1
+    else
+	echo 'Already a configuration exists ...'
+	echo 'Check the configuration directory ' $MY_CONFIG_PATH'/'$MY_CONFIG_NAM
+	copy_tag=0
+    fi
+else
+    if [ ! -d $MY_CONFIG_PATH'/'$MY_CONFIG_NAME ]; then
+	mkdir $MY_CONFIG_PATH'/'$MY_CONFIG_NAME
+	copy_tag=1
+    else
+	echo 'Already a configuration exists ...'
+	echo 'Check the configuration directory ' $MY_CONFIG_PATH'/'$MY_CONFIG_NAM
+	copy_tag=0
+    fi
 fi
-# Link the files from croco/
-echo '=> Copy the file from '$SOURCES_DIR ' and ' $TOOLS_DIR 
-echo '   needed to setup your own simulations'
-echo '         '
 
-cd $MY_CONFIG_PATH'/'MY_CONFIG_NAME
-#
-cp -Rf $SOURCES_DIR/jobcomp
-cp -Rf $SOURCES_DIR/cppdef.h .
-cp -Rf $SOURCES_DIR_DIR/param.h .
-cp -Rf $SOURCES_DIR/*.in .
-
-#TEST_CASES
-cp -Rf $SOURCES_DIR/TEST_CASES .
-cp -Rf $SOURCES_DIR/ROMS_FILES .
-
-#NAMELIST_OANALYSIS
-cp -Rf $SOURCES_DIR/$NAMELIST_OANALYSIS .
-
-#XIOS
-cp -Rf $SOURCES_DIR/domain_def.xml .
-cp -Rf $SOURCES_DIR/field_def.xml_full .
-cp -Rf $SOURCES_DIR/xios_launch.file .
-
-echo '=> Copy from '$SOURCES_DIR ' done'
-echo '         '
-# Link the files from croco_tools/
-# for romstools in matlab
-cp -Rf $TOOLS_DIR/start.m .
-cp -Rf $TOOLS_DIR/romstools_param.m .
-ln -sf $TOOLS_DIR/*
-
-#
-echo '=> Copy from '$TOOLS_DIR ' done'
-
+if [[ $copy_tag == 1 ]] ; then
+    
+# Copy the files from croco/
+    echo '=> Copy the file from '$SOURCES_DIR ' and ' $TOOLS_DIR 
+    echo '   needed to setup your own simulations'
+    echo '         '
+    
+    
+    cd $MY_CONFIG_PATH'/'$MY_CONFIG_NAME
+    
+    
+    #OCEAN
+    cp -Rf $SOURCES_DIR/OCEAN/jobcomp .
+    cp -Rf $SOURCES_DIR/OCEAN/param.h . 
+    cp -Rf $SOURCES_DIR/OCEAN/cppdefs.h . 
+    cp -Rf $SOURCES_DIR/OCEAN/roms.in . 
+    cp -Rf $SOURCES_DIR/OCEAN/roms.in.1 . 
+    cp -Rf $SOURCES_DIR/OCEAN/roms_inter.in .
+    cp -Rf $SOURCES_DIR/OCEAN/sediment.in .
+    cp -Rf $SOURCES_DIR/OCEAN/roms_forecast.in .
+    cp -Rf $SOURCES_DIR/OCEAN/roms_hindcast.in .
+    cp -Rf $SOURCES_DIR/OCEAN/roms_stations.in .
+    
+    # TEST_CASE + NAMELIST_OANALYSIS
+    cp -Rf $SOURCES_DIR/OCEAN/TEST_CASES .
+    cp -Rf $SOURCES_DIR/OCEAN/NAMELIST_OANALYSIS .
+    
+    # XIOS
+    cp -Rf $SOURCES_DIR/XIOS/domain_def.xml .
+    cp -Rf $SOURCES_DIR/XIOS/field_def.xml_full .
+    cp -Rf $SOURCES_DIR/XIOS/xios_launch.file .
+    cp -Rf $SOURCES_DIR/XIOS/README_XIOS .
+ 
+    echo '=> Copy from '$SOURCES_DIR ' done'
+    echo '         '
+    # Link the files from croco_tools/
+    # for romstools in matlab
+    cp -Rf $TOOLS_DIR/start.m .
+    cp -Rf $TOOLS_DIR/romstools_param.m .
+    cp -Rf $TOOLS_DIR/Misc/town.dat .
+    #
+    cd -
+    #
+    echo '=> Copy from '$TOOLS_DIR ' done'
+    
+fi
