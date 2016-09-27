3f8f50060bd50f56753f69a1c2980b254ef87eda
crocodev <crocodev@service7.ice.ifremer.fr>
2016-09-27 09:00:36 +0000

    Remove rm -Rf jobcomp_rvtk.bash and replace it by a renaming to jobcomp_rvtk.bash.BACK

--- CVTK/test_repro/RVTK_DEBUG/clean_rvtkdir.sh
+++ CVTK/test_repro/RVTK_DEBUG/clean_rvtkdir.sh
@@ -3,9 +3,9 @@ echo '============================================================='
 echo 'Clean RVTK_DIR '
 echo '  '
 rm -Rf ROMS_FILES VHR RIP VORTEX JET SHOREFACE TEST_CASES Compile Scripts
-rm -Rf roms.in* rvtk_* jobcomp_rvtk.bash *.h Recap* *.exe *.log *.pbs AGRIF_FixedGrids.in*
+rm -Rf roms.in* rvtk_* *.h Recap* *.exe *.log *.pbs AGRIF_FixedGrids.in*
 rm -Rf check_file  kRGB61.txt svninfos gitinfos namelist.trc.sms
 rm -Rf Log *.slurm test_croco.sh test_roms.sh gitinfo.sh  extract_results_* 
-
+mv jobcomp_rvtk.bash jobcomp_rvtk.bash.BACK
 echo 'Well done: Cleaning achieved'
 echo '============================================================='
