1b4f2c60db42658b415c6699ebfe3e8ec079c06a
Gildas Cambon <gildas.cambon@ird.fr>
2018-02-12 18:11:29 +0000

    Dev ...

--- CVTK/test_repro/RVTK_DEBUG_FAST_src/Scripts_reg/src_test/create_link_for_Test_reg.sh
+++ CVTK/test_repro/RVTK_DEBUG_FAST_src/Scripts_reg/src_test/create_link_for_Test_reg.sh
@@ -6,7 +6,9 @@ dir_testroot=/home7/datawork/crocodev/RVTK_DEBUG_REG_DEV/TESTROOT/REG
 dir_datafiles=$DATAWORK/CROCO_FILES_VHR_BCK
 
 #echo 'Process input file'
+if [ ! -d CROCO_FILES ] ; then 
 ln -sf $dir_datafiles CROCO_FILES
+fi
 ln -sf $dir_testroot/VHR .
 ln -sf $dir_testroot/test_croco_reg.sh .
 ln -sf $dir_testroot/rvtk_fast_qsub_REGIONAL.bash .
