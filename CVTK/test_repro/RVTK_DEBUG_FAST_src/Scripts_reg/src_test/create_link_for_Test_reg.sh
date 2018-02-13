d49f85e432c5596f7d87e6de3571b0f0ed398d5c
Gildas Cambon <gildas.cambon@ird.fr>
2018-02-13 13:36:47 +0000

    Update

--- CVTK/test_repro/RVTK_DEBUG_FAST_src/Scripts_reg/src_test/create_link_for_Test_reg.sh
+++ CVTK/test_repro/RVTK_DEBUG_FAST_src/Scripts_reg/src_test/create_link_for_Test_reg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo '============================================================='
 echo 'Create the link between TESTROOT . and '$PWD
-set -x
+#set -x
 dir_testroot=/home7/datawork/crocodev/RVTK_DEBUG_REG_DEV/TESTROOT/REG
 dir_datafiles=$DATAWORK/CROCO_FILES_VHR_BCK
 
@@ -16,7 +16,7 @@ ln -sf $dir_testroot/jobcomp_rvtk.bash .
 ln -sf $dir_testroot/extract_results_croco.bash .
 
 #echo 'Process namelist files'
-ln -sf ${dir_home}/VHR/AGRIF_FixedGrids.in.REGIONAL.VHR AGRIF_FixedGrids.in.REGIONAL
+ln -sf VHR/AGRIF_FixedGrids.in.REGIONAL.VHR AGRIF_FixedGrids.in.REGIONAL
 ln -sf VHR/croco.in.VHR.1 croco.in.1
 ln -sf VHR/croco.in.VHR croco.in
 
