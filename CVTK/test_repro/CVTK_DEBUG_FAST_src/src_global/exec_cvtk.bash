96009218f5ab4df22873ccee26ab6fc0d2f4a522
Gildas Cambon <gildas.cambon@ird.fr>
2018-04-26 13:53:45 +0000

    Update the CVTK_FAST test suite

--- CVTK/test_repro/CVTK_DEBUG_FAST_src/src_global/exec_cvtk.bash
+++ CVTK/test_repro/CVTK_DEBUG_FAST_src/src_global/exec_cvtk.bash
@@ -8,14 +8,19 @@ cd $PBS_O_WORKDIR
 echo $PBS_O_LOGNAME
 #===================================
 
+#-----------------------
+source CONFIGURE_GLOBAL
+#-----------------------
+
 cd ${TESTROOTDIR}/REG
 ./mk_TESTALL_reg.bash
 cd -
-
+#==
 cd ${TESTROOTDIR}/VORT
 ./mk_TESTALL_vort.bash
 cd -
-
+#==
 cd ${TESTROOTDIR}/KTEST
 ./mk_TESTALL_ana.bash
 cd -
+#=
--- CVTK/test_repro/CVTK_DEBUG_FAST_src/src_global/gather_all.bash
+++ CVTK/test_repro/CVTK_DEBUG_FAST_src/src_global/gather_all.bash
@@ -4,9 +4,15 @@
 #PBS -l walltime=02:00:00
 #PBS -j oe
 #PBS -M gildas.cambon@ird.fr -m abe
+
 cd $PBS_O_WORKDIR
 echo $PBS_O_LOGNAME
-#===================================
+###===================================
+
+#-----------------------
+source CONFIGURE_GLOBAL
+#-----------------------
+set -x
 
 cd ${TESTROOTDIR}/REG
 ./gather_recap.bash REG
