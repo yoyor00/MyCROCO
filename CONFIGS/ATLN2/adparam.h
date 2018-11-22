264961bf60279d9cc67bb2d4a85ce8357a41134e
Maurice Bremond <Maurice.Bremond@inria.fr>
2018-11-22 16:51:43 +0100

    [AD] config with random collocation points

--- CONFIGS/ATLN2/adparam.h
+++ CONFIGS/ATLN2/adparam.h
@@ -2,7 +2,7 @@ C     -*- fortran -*-
 
 C     size of the problem (number of control variables)
       integer ad_array_size
-      parameter (ad_array_size=(3+Lm+3+padd_X)*(3+Mm+3+padd_E))
+      parameter (ad_array_size=30)
 c      parameter (ad_array_size=(lm+1+padd_x)*(mm+1+padd_e))
 
 C     size of the assimilation window (number of steps)
@@ -24,12 +24,26 @@ C     observations
 C     control vector
       double precision ad_x(ad_array_size)
 
+c     coordinates of control points (i.e collocation points)
+      integer ad_i(ad_array_size)
+      integer ad_j(ad_array_size)
+
+c     weighted coefficients
+      double precision W(GLOBAL_2D_ARRAY,ad_array_size)
+      double precision SkW(GLOBAL_2D_ARRAY)
+      
 C     tangential vector
       double precision ad_xd(ad_array_size)
 
 C     time step of the main simulation
       integer sim_iicroot
 
+c     tidal period (M2)
+      double precision TM2
+      parameter(TM2 = 12.4206012)
+      
 C     commons
+      common /collocation_coords/ ad_i,ad_j
+      common /weighted_coefs/ W,SkW
       common /obs_data/ ad_obs
       common /state_info/ sim_iicroot
