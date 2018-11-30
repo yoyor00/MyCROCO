31fe57b0b97e4a912f952ea7c9f6966ede77fc57
Maurice Bremond <Maurice.Bremond@inria.fr>
2018-11-30 13:36:05 +0100

    [AD] add adinc.h for common includes

---
+++ adinc.h
@@ -0,0 +1,21 @@
+c -*- fortran -*-
+
+# include "cppdefs.h"
+# include "param.h"
+# include "grid.h"
+# include "scalars.h"
+# include "ocean2d.h"
+# include "ncscrum.h"
+# include "private_scratch.h"
+      real*QUAD buff(6)
+      common /xyz/ buff
+# include "climat.h"
+# include "forces.h"
+# ifdef MPI
+# include "mpi_roms.h"
+# ifdef AMPI
+# include "ampi/ampif.h"
+# else
+      include 'mpif.h'
+# endif
+# endif
