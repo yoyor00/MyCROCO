42abb85901c1fac644bcb488a7c2b58c3a819fc8
Maurice Bremond <Maurice.Bremond@inria.fr>
2019-01-31 09:52:40 +0100

    [AD] collocation points visualization

---
+++ CONFIGS/ATLN3/cplot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.basemap import Basemap
+
+#points=np.loadtxt('fort.778')
+points=np.loadtxt('coords.dat')
+
+m = Basemap(projection='merc', llcrnrlon=-9, urcrnrlon=1,llcrnrlat=43,urcrnrlat=51, resolution='h')
+ 
+# draw coastlines and borders
+m.drawcoastlines()
+m.drawcountries()
+ 
+# draw meridians and parallels
+m.drawmeridians(np.arange(0, 360, 30))
+m.drawparallels(np.arange(-90, 90, 30))
+for x,y in points:
+    print (x,y)
+    m.plot(x,y,'o',latlon=True)
+plt.show()
