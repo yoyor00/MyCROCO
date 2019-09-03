0d710a7e83e771c1e5032e250c839a3ecfa8d89d
Rachid Benshila <rachid.benshila@legos.obs-mip.fr>
2019-09-03 22:44:37 +0200

    correction for gitlab-ci

--- CVTK/test_repro/CVTK_DEBUG_FAST_src/Scripts_ana/src_test/mk_TESTALL_ana.bash
+++ CVTK/test_repro/CVTK_DEBUG_FAST_src/Scripts_ana/src_test/mk_TESTALL_ana.bash
@@ -20,7 +20,7 @@ done
 
 found=0
 for testconf in `ls -1 ./Configure_Test/ `; do
-  mytest=$(ls $testconf/jobcomp_OPENMP* 2>&1)
+  mytest=$(ls $testconf/jobcomp_OPENMP* > /dev/null 2>&1)
   if [ $? -eq 1 ]; then
     if [ $found -eq 0 ]; then
       echo -e "Some configurations were not tested :" 
@@ -28,7 +28,7 @@ for testconf in `ls -1 ./Configure_Test/ `; do
     fi   
     echo -e "   - $testconf not tested for OPENMP"    
   fi  
-  mytest=$(ls $testconf/jobcomp_MPI* 2>&1)
+  mytest=$(ls $testconf/jobcomp_MPI* > /dev/null 2>&1)
   if [ $? -eq 1 ]; then
     if [ $found -eq 0 ]; then
       echo -e "Some configurations were not tested :" 
