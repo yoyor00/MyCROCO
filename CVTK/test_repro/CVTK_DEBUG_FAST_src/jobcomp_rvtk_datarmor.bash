ff73acd4920a458afa37153d06c56247e01b3936
cambon <gildas.cambon@ird.fr>
2019-09-06 16:34:45 +0200

    CI

--- CVTK/test_repro/CVTK_DEBUG_FAST_src/jobcomp_rvtk_datarmor.bash
+++ CVTK/test_repro/CVTK_DEBUG_FAST_src/jobcomp_rvtk_datarmor.bash
@@ -42,12 +42,12 @@ echo "OPERATING SYSTEM IS: $OS"
 #
 # compiler options
 #
-FC=CI_FC
+FC=$CI_FC
 
 #
 # set MPI directories if needed
 #
-MPIF90=CI_MPIF90
+MPIF90=$CI_MPIF90
 MPILIB=""
 MPIINC=""
 
