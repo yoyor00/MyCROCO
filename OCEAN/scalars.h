3acdc520b498e35567d711355e5c29ca0605696d
pvthinker <roullet@univ-brest.fr>
2020-04-05 14:57:13 +0200

    Fix typo
    comma was misplaced

--- OCEAN/scalars.h
+++ OCEAN/scalars.h
@@ -58,7 +58,7 @@
      &                       iif, nstp, nrhs, nnew, nbstep3d,
 #endif
 #ifdef NHMG
-     &      , iprec1, iprec2
+     &                       iprec1, iprec2,
 #endif
 #ifdef FLOATS
      &                       nfp1, nf, nfm1, nfm2, nfm3,
