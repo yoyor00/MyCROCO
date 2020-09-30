ea843b3d8ca05b7c918142dad18d6a62d27bd3a1
rblod <rachid.benshila@legos.obs-mip.fr>
2020-09-30 17:36:05 +0200

    Add variables for cohesive bed and floculation

--- OCEAN/sediment.h
+++ OCEAN/sediment.h
@@ -75,10 +75,12 @@
       real bed_thick(GLOBAL_2D_ARRAY,NLAY),
      &     bed_poros(GLOBAL_2D_ARRAY,NLAY),
      &     bed_age  (GLOBAL_2D_ARRAY,NLAY),
+     &     bed_bctr (GLOBAL_2D_ARRAY,NLAY),
      &     bed_mass (GLOBAL_2D_ARRAY,NLAY,2,NST),
      &     worksed_bed(GLOBAL_2D_ARRAY,NLAY)
       common /sediment_bed/ bed_thick, bed_poros,
      &                      bed_age  , bed_mass,
+     &                      bed_bctr ,
      &                      worksed_bed
 
       real bed_frac(GLOBAL_2D_ARRAY,NLAY,NST),
@@ -88,11 +90,20 @@
       real bot_thick(GLOBAL_2D_ARRAY)
       common /bot_thick/ bot_thick 
 
+      real bot_dnet(GLOBAL_2D_ARRAY)
+      common /bot_dnet/ bot_dnet 
+
+      real bot_dprp(GLOBAL_2D_ARRAY)
+      common /bot_dprp/ bot_dprp 
+
       real bedload_coeff, morph_fac
       common /bed_coeff/ bedload_coeff, morph_fac 
 
       real tau_ce_2d(GLOBAL_2D_ARRAY,NST)
       common /hidexp/ tau_ce_2d
+
+      real minlayer_thick
+      common /layer_thick/ minlayer_thick
   
 # ifdef SUSPLOAD
       real settling_flux(GLOBAL_2D_ARRAY,NST)
@@ -122,6 +133,61 @@
       real bedldv_avg(GLOBAL_2D_ARRAY,NST)
       common /sed_bedload_avg/ bedldu_avg, bedldv_avg
 #  endif
+# endif
+
+#if defined COHESIVE_BED || defined MIXED_BED
+      real tcr_min, tcr_max, tcr_slp, tcr_off, tcr_tim
+      common /sed_tcr/ tcr_min, tcr_max, tcr_slp, tcr_off, tcr_tim
+#endif
+
+#if defined MIXED_BED
+      real transC, transN
+      common /sed_trans/ transC, transN
+#endif
+
+# if defined SED_FLOCS
+      real mud_frac_eq(NMUD)
+      real t_dfloc
+      common /sedfloc_bed/ mud_frac_eq, t_dfloc
+
+      logical l_ASH
+      logical l_ADS
+      logical l_COLLFRAG
+      logical l_testcase
+      integer f_ero_iv
+      real f_dp0,f_alpha,f_beta,f_nb_frag
+      real f_dmax,f_ater,f_clim
+      real f_ero_frac,f_ero_nbfrag
+      real f_nf
+      real f_frag
+      real f_fter
+      real f_collfragparam
+      common /sedflocs1/ l_ASH,l_ADS,l_COLLFRAG,
+     & l_testcase,f_dp0,f_alpha,f_beta,f_nb_frag,
+     & f_dmax,f_ater,f_clim,f_ero_frac,f_ero_nbfrag,
+     & f_nf, f_frag,f_fter,f_collfragparam,f_ero_iv
+
+      real rhoref
+      parameter  (rhoref = 1030.0_r8)
+
+      real f_diam(NMUD)
+      real f_vol(NMUD)
+      real f_rho(NMUD)
+      real f_cv(NMUD)
+      real f_l3(NMUD)
+      real f_mass(0:NMUD+1)
+      real f_coll_prob_sh(NMUD,NMUD)
+      real f_coll_prob_ds(NMUD,NMUD)
+      real f_l1_sh(NMUD,NMUD)
+      real f_l1_ds(NMUD,NMUD)
+      real f_g3(NMUD,NMUD)
+      real f_l4(NMUD,NMUD)
+      real f_g1_sh(NMUD,NMUD,NMUD)
+      real f_g1_ds(NMUD,NMUD,NMUD)
+      real f_g4(NMUD,NMUD,NMUD)    
+      common /sedflocs2/ f_diam,f_vol,f_rho,f_cv,f_l3,
+     &   f_mass,f_coll_prob_sh,f_coll_prob_ds,
+     &   f_l1_sh,f_l1_ds,f_g3,f_l4,f_g1_sh,f_g1_ds,f_g4 
 # endif
 
       character*80 Stitle
