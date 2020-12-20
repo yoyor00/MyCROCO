9ac17fac06efebf19554e7ff65c1055a901326fd
gula <gula@univ-brest.fr>
2020-12-20 15:35:49 +0100

    Simplify sequence by writing rst file  in OUT_seg12 in the first phase and reading directly from there in the second one.

--- CVTK/test_perfrst/test_debug_perfrst.bash
+++ CVTK/test_perfrst/test_debug_perfrst.bash
@@ -3,18 +3,18 @@
 # VERIFIER QUES LES CPPDEFS.H SONT LES MEMES!!
 
 #0
-#./compile_croco_PR.bash
+./compile_croco_PR.bash
 
 #1
 rm -Rf check_file_1_0
+mkdir OUT OUT_seg12
 rm -Rf OUT/* OUT_seg12/*
 
 #2
 #./croco_EXACT_RST_DBWRITE croco.in.rst_seg12_PR
 mpirun -np 4 ./croco_EXACT_RST_DBWRITE croco.in.rst_seg12_PR
-#3
-rm -Rf CROCO_FILES/*rst* ; cp -rf OUT_seg12/croco_rst* CROCO_FILES/
 
-#4
+
+#3
 #./croco_EXACT_RST_DBREAD croco.in
 mpirun -np 4 ./croco_EXACT_RST_DBREAD croco.in
