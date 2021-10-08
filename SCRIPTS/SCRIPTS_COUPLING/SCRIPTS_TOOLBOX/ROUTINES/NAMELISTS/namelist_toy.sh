600b754622687cd6c8b4e26512a0215d5a886768
mathieulc <mathieu.lecorre@univ-brest.fr>
2021-10-08 15:26:27 +0000

    Update path for toy input

--- SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/ROUTINES/NAMELISTS/namelist_toy.sh
+++ SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/ROUTINES/NAMELISTS/namelist_toy.sh
@@ -5,6 +5,6 @@
 export toytype=("wav" "atm") #oce,atm,wav
 
 # forcing files
-export toyfile=("$CWORK/toy_files/ww3.frc.200501.nc" "$CWORK/toy_files/wrfout_d01_20050101_20050131.nc")
+export toyfile=("$CWORK/TOY_FILES/ww3.frc.200501.nc" "$CWORK/TOY_FILES/wrfout_d01_20050101_20050131.nc")
 export timerange=('1,745' '2,125')
 
