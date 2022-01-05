5190e02aad896d3b1da335552bea7ae21e17bd2e
mathieulc <mathieu.lecorre@univ-brest.fr>
2022-01-05 17:44:46 +0000

    Update default xios output files

--- SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/NAMELISTS/namelist_xios.sh
+++ SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/NAMELISTS/namelist_xios.sh
@@ -7,5 +7,5 @@ export ONLINE_XML="FALSE" # Process xml online
 # name of xml files (defined in file_def_*). Here are default names in xml files.
 export ATM_XIOS_NAME="wrfout wrf3d_1D wrf3d_1H" # All the names you have defined in your xml file
 export OCE_OUTPUT_PREFIX="croco"
-export OCE_XIOS_NAME="${OCE_OUTPUT_PREFIX}_1d_aver ${OCE_OUTPUT_PREFIX}_1h_inst_surf ${OCE_OUTPUT_PREFIX}_6h_his"
+export OCE_XIOS_NAME="${OCE_OUTPUT_PREFIX}_3h_inst ${OCE_OUTPUT_PREFIX}_1h_avg_3d ${OCE_OUTPUT_PREFIX}_1h_inst_surf ${OCE_OUTPUT_PREFIX}_5d_aver" # Relative to what is in the file_def_croco.xml
 
