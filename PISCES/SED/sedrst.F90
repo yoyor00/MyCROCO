361b0860c010867a7d3834fa1225e52d85b87f7e
Gildas Cambon <gildas.cambon@ird.fr>
2022-01-14 15:13:01 +0100

    Typo

--- PISCES/SED/sedrst.F90
+++ PISCES/SED/sedrst.F90
@@ -329,7 +329,7 @@ CONTAINS         ! Write model prognostic
 #endif
 
         IF (ierr .NE. nf_noerr) THEN
-           WRITE(stdout,'(/3(1x,A)/)') 'ERROR in DEF_RST_SED: Cannot',
+           WRITE(stdout,'(/3(1x,A)/)') 'ERROR in DEF_RST_SED: Cannot', & 
      &         'create restart NetCDF file:', TRIM(cn_sedrst_out)
            GOTO 99                                         !--> ERROR
         ENDIF
