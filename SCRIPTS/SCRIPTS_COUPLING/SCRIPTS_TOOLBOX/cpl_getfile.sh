ce97d5007728fd0faed2c63d92c31aae66590b82
mathieulc <mathieu.lecorre@univ-brest.fr>
2023-02-16 08:32:32 +0000

    Solve time units format when using ANA_INITIAL

--- SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/cpl_getfile.sh
+++ SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/cpl_getfile.sh
@@ -6,6 +6,6 @@
 if [[ ${WEIGHT_FLAG} == 1 ]]; then
     weight_files="${weight_o2a} ${weight_a2o}"
     for file in ${weight_files}; do
-        ${io_getfile}  ${CWORK}/OASIS_FILES/${file} .
+        ${io_getfile}  ${CPL_FILES_DIR}/${file} .
     done
 fi
