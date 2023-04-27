f03e437b49c8967a0818216d0419a5a6cce8364b
Francis AUCLAIR <francis.auclair@aero.obs-mip.fr>
2023-04-27 16:32:22 +0200

    AGRIF (10b).

--- NBQ2/m3fast_post.h
+++ NBQ2/m3fast_post.h
@@ -4,28 +4,6 @@
 ! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! !
 ! !********************************
-! ! Update ubar and vbar 
-! !     and their BC (AGRIF)
-! ! BC for DU(V)_nbq treated here
-! !********************************
-! !
-      do j=Jstr,Jend
-        do i=IstrU,Iend
-          ubar(i,j,knew)=urhs(i,j)
-        enddo
-      enddo
-      do j=JstrV,Jend
-        do i=Istr,Iend
-          vbar(i,j,knew)=vrhs(i,j)
-        enddo
-      enddo
-
-      M2bc_nbq_flag=.true. ! apply boundary wet/dry conditions
-                           ! and boundaries for DU(V)_nbq
-      call u2dbc_tile   (Istr,Iend,Jstr,Jend, work)
-      call v2dbc_tile   (Istr,Iend,Jstr,Jend, work)
-! !
-! !********************************
 ! !  Update total mass of water 
 ! !    volume Dnew 
 ! !********************************
