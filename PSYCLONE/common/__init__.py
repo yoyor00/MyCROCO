c3a381d0e8fcd112b1eee99dbfddc9b070019300
Sebastien Valat <sebastien.valat.dev@orange.fr>
2023-10-24 21:48:08 +0200

    [psyclone] Update author lab

--- PSYCLONE/common/__init__.py
+++ PSYCLONE/common/__init__.py
@@ -1,5 +1,5 @@
 ##########################################################
 #  CROCO PSYCLONE scripts, under CeCILL-C
-#  From Sébastien Valat (INRIA) - 2023
+#  From Sébastien Valat (INRIA & LJK) - 2023
 #  CROCO website : http://www.croco-ocean.org
 ##########################################################
