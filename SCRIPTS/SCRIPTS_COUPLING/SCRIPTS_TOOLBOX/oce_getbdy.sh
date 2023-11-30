c43687c33298132fc1eee730607a9e9f65694383
LE GAC <solene.le.gac@ifremer.fr>
2023-11-30 16:45:51 +0100

    Fix #126 : remove sc_r and sc_w in output, change Cs_r to Cs_rho

--- SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/oce_getbdy.sh
+++ SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/oce_getbdy.sh
@@ -13,7 +13,7 @@ fi
 # put 1-d stuff inside bdy file
 cur_Y=$( echo $DATE_BEGIN_JOB | cut -c 1-4 )
 cur_M=$( echo $DATE_BEGIN_JOB | cut -c 5-6 )
-varlist="spherical,Vtransform,Vstretching,tstart,tend,theta_s,theta_b,Tcline,hc,sc_r,sc_w,Cs_r,Cs_w"
+varlist="spherical,Vtransform,Vstretching,tstart,tend,theta_s,theta_b,Tcline,hc,Cs_rho,Cs_w"
 ncks -A -v "${varlist}"  ${OCE_FILES_DIR}/croco_${bdy_ext}_Y${cur_Y}M${cur_M}.nc ${bryfile}
 #
 options=("temp" "salt" "v2d" "v3d"  "zeta")
