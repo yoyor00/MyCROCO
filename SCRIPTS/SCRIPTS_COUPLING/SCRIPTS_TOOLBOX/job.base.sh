db696cad22b165be35594e46dd62cc380ac13527
Swen JULLIEN <swen.jullien@ifremer.fr>
2024-02-03 18:50:08 +0000

    COUPLING : correct typo in MPI launch commande in job.base. Fix issue #135

--- SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/job.base.sh
+++ SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/job.base.sh
@@ -125,10 +125,10 @@ cd ${EXEDIR}
         printf "\n ***************** make app.conf file for Multiple Program Multiple Data *****************\n\n"
         [ -f ${SCRIPTDIR}/MACHINE/${MACHINE}/launch_${MACHINE}.sh ] && { . ${SCRIPTDIR}/MACHINE/${MACHINE}/launch_${MACHINE}.sh ; } || { printf "\n Please create a file launch_${MACHINE}.sh \n" ; exit 0 ; }
 
-	echo "launch run: $MPI_LAUNCH ${MPI_ext} app.conf " #${run_cmd} #$app.conf
+	echo "launch run: $MPI_LAUNCH_CMD ${MPI_ext} app.conf " #${run_cmd} #$app.conf
 
 ##### RUN ######
-time $MPI_LAUNCH ${MPI_ext} app.conf >& out_run.txt 
+time $MPI_LAUNCH_CMD ${MPI_ext} app.conf >& out_run.txt 
 [ "$?" -gt "0" ] && { printf "ERROR during the RUN.\n Please check le log.files in ${EXEDIR} (out_run.txt, croco.log,...)"; exit ; }
 ################
 #        fi
