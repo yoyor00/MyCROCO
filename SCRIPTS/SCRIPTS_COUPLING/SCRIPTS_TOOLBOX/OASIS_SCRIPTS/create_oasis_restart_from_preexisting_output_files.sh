2c1793bd82286f6295361aeacfb607579d310f78
Swen JULLIEN <swen.jullien@ifremer.fr>
2024-02-29 13:34:47 +0000

    CPL scripts : some correction in create_oasis_restart_from_preexisting_output_files.sh in the case of OW_COUPLING_FULL

--- SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/OASIS_SCRIPTS/create_oasis_restart_from_preexisting_output_files.sh
+++ SCRIPTS/SCRIPTS_COUPLING/SCRIPTS_TOOLBOX/OASIS_SCRIPTS/create_oasis_restart_from_preexisting_output_files.sh
@@ -115,15 +115,32 @@ elif  [ $model == ww3 ] ; then
 #            WW3_TAWX \
 #            WW3_TAWY \
 #            WW3__CHA)
-
-    varlist=(WW3_T0M1 \
-            WW3___HS \
-            WW3__DIR \
-            WW3_TWOX \
-            WW3_TWOY \
-            WW3_TAWX \
-            WW3_TAWY \
-            WW3_ACHA)
+    if [ $OW_COUPLING_FULL = 'TRUE' ] ; then
+        varlist=(WW3_T0M1 \
+                WW3__OHS \
+                WW3__DIR \
+                WW3_TWOX \
+                WW3_TWOY \
+                WW3_TAWX \
+                WW3_TAWY \
+                WW3_ACHA \
+                WW3__FOC \
+                WW3_USSX \
+                WW3_USSY \
+                WW3___LM \
+                WW3__BHD \
+                WW3_UBRX \
+                WW3_UBRY)
+    else
+        varlist=(WW3_T0M1 \
+                WW3__OHS \
+                WW3__DIR \
+                WW3_TWOX \
+                WW3_TWOY \
+                WW3_TAWX \
+                WW3_TAWY \
+                WW3_ACHA)
+    fi
 
     dimtime=time
     timerange=1
@@ -148,11 +165,13 @@ for k in `seq 0 $(( ${lengthvar} - 1))` ; do
 
     # Extract or compute var
     echo '---> Extract or compute '$var
+    echo "${SCRIPTDIR}/OASIS_SCRIPTS/from_${model}.sh $filein $filetmp $var $timerange $gridlevels"
     ${SCRIPTDIR}/OASIS_SCRIPTS/from_${model}.sh $filein $filetmp $var $timerange $gridlevels
 
     if [ $model == wrf ] ; then
         # Put them on the stag grid
-        echo '---> Put them on the stag grid' 
+        echo '---> Put them on the stag grid'
+        echo "${SCRIPTDIR}/OASIS_SCRIPTS/to_wrf_stag_grid.sh $filetmp $filetmp" 
         ${SCRIPTDIR}/OASIS_SCRIPTS/to_wrf_stag_grid.sh $filetmp $filetmp
     fi
 
