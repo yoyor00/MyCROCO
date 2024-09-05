2134ce30502bc47ba6050bc3807300831ee7abfe
LE GAC <solene.le.gac@ifremer.fr>
2024-09-05 17:23:54 +0200

    #190 adjust number on cpus to runner capabilities

--- TEST_CASES/CPL_TOY/run_mpirun.sh
+++ TEST_CASES/CPL_TOY/run_mpirun.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # Run the mpirun command with redirection
-mpirun --allow-run-as-root -np 2 toywav : -np 4 crocox > out.log 2>&1
+mpirun --allow-run-as-root -np 2 toywav : -np 2 crocox > out.log 2>&1
+
+# Check if the command was successful
+if [ $? -ne 0 ]; then
+  echo "Error: mpirun command failed" >&2
+  exit 1
+fi
+
+echo "mpirun command completed successfully."
