2b641d2a74e15f916d16231c33c467e536349543
Rachid Benshila <rachid.benshila@univ-tlse3.fr>
2025-01-13 21:03:53 +0100

    add plot_river.py

--- TEST_CASES/croco_utils.py
+++ TEST_CASES/croco_utils.py
@@ -90,3 +90,119 @@ def get_csf(sc, theta_s, theta_b):
         CSF = csrf
 
     return CSF
+
+def rho2u_2d(rho_in):
+    '''
+    Convert a 2D field at rho points to a field at u points
+    '''
+    def _r2u(rho_in, Lp):
+        u_out = rho_in[:, :Lp - 1]
+        u_out += rho_in[:, 1:Lp]
+        u_out *= 0.5
+        return u_out.squeeze()
+    assert rho_in.ndim == 2, 'rho_in must be 2d'
+    Mshp, Lshp = rho_in.shape
+    return _r2u(rho_in, Lshp)
+
+def rho2u_3d(rho_in):
+    '''
+    Convert a 3D field at rho points to a field at u points
+    Calls rho2u_2d
+    '''
+    def levloop(rho_in):
+        Nlevs, Mshp, Lshp = rho_in.shape
+        rho_out = np.zeros((Nlevs, Mshp, Lshp-1))
+        for k in xrange(Nlevs):
+             rho_out[k] = ROMS.rho2u_2d(rho_in[k])
+        return rho_out
+    assert rho_in.ndim == 3, 'rho_in must be 3d'
+    return levloop(rho_in)
+
+def rho2v_2d(rho_in):
+    '''
+    Convert a 2D field at rho points to a field at v points
+    '''
+    def _r2v(rho_in, Mp):
+        v_out = rho_in[:Mp - 1]
+        v_out += rho_in[1:Mp]
+        v_out *= 0.5
+        return v_out.squeeze()
+    assert rho_in.ndim == 2, 'rho_in must be 2d'
+    Mshp, Lshp = rho_in.shape
+    return _r2v(rho_in, Mshp)
+
+def rho2v_3d(rho_in):
+    '''
+    Convert a 3D field at rho points to a field at v points
+    Calls rho2v_2d
+    '''
+    def levloop(rho_in):
+        Nlevs, Mshp, Lshp = rho_in.shape
+        rho_out = np.zeros((Nlevs, Mshp-1, Lshp))
+        for k in xrange(Nlevs):
+             rho_out[k] = ROMS.rho2v_2d(rho_in[k])
+        return rho_out
+    assert rho_in.ndim == 3, 'rho_in must be 3d'
+    return levloop(rho_in)
+
+def u2rho_2d(u_in):
+    '''
+    Convert a 2D field at u points to a field at rho points
+    '''
+    def _uu2ur(uu_in, Mp, Lp):
+        L, Lm = Lp - 1, Lp - 2
+        u_out = np.zeros((Mp, Lp))
+        u_out[:, 1:L] = 0.5 * (u_in[:, 0:Lm] + \
+                               u_in[:, 1:L])
+        u_out[:, 0] = u_out[:, 1]
+        u_out[:, L] = u_out[:, Lm]
+        return u_out.squeeze()
+
+    assert u_in.ndim == 2, 'u_in must be 2d'
+    Mp, Lp = u_in.shape
+    return _uu2ur(u_in, Mp, Lp+1)
+
+def u2rho_3d(u_in):
+    '''
+    Convert a 3D field at u points to a field at rho points
+    Calls u2rho_2d
+    '''
+    def _levloop(u_in):
+        Nlevs, Mshp, Lshp = u_in.shape
+        u_out = np.zeros((Nlevs, Mshp, Lshp+1))
+        for Nlev in xrange(Nlevs):
+            u_out[Nlev] = ROMS.u2rho_2d(u_in[Nlev])
+        return u_out
+    assert u_in.ndim == 3, 'u_in must be 3d'
+    return _levloop(u_in)
+
+def v2rho_2d(v_in):
+    '''
+    Convert a 2D field at v points to a field at rho points
+    '''
+    def _vv2vr(v_in, Mp, Lp):
+        M, Mm = Mp - 1, Mp - 2
+        v_out = np.zeros((Mp, Lp))
+        v_out[1:M] = 0.5 * (v_in[:Mm] + \
+                               v_in[1:M])
+        v_out[0] = v_out[1]
+        v_out[M] = v_out[Mm]
+        return v_out.squeeze()
+
+    assert v_in.ndim == 2, 'v_in must be 2d'
+    Mp, Lp = v_in.shape
+    return _vv2vr(v_in, Mp+1, Lp)
+
+def v2rho_3d(v_in):
+    '''
+    Convert a 3D field at v points to a field at rho points
+    Calls v2rho_2d
+    '''
+    def levloop(v_in):
+        Nlevs, Mshp, Lshp = v_in.shape
+        v_out = np.zeros((Nlevs, Mshp+1, Lshp))
+        for Nlev in xrange(Nlevs):
+            v_out[Nlev] = ROMS.v2rho_2d(v_in[Nlev])
+        return v_out
+    assert v_in.ndim == 3, 'v_in must be 3d'
+    return levloop(v_in)
