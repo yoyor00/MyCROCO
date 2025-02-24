14241fc5f8768761146a83dc0697500ce96d7035
Solène Le Gac <solene.le.gac@ifremer.fr>
2025-02-24 15:16:59 +0100

    Resolve "MPI repro issue with NBQ"

--- OCEAN/cppdefs.h
+++ OCEAN/cppdefs.h
@@ -123,11 +123,6 @@
 #  undef  MPI_TIME
 # endif
 # undef  AUTOTILING
-                      /* Non-hydrostatic options */
-# ifdef NBQ
-#  define W_HADV_TVD
-#  define W_VADV_TVD
-# endif
                       /* Grid configuration */
 # define CURVGRID
 # define SPHERICAL
@@ -1440,7 +1435,7 @@
 !      Tracer dispersion by surfzone eddies: assessing 
 !      the impact of undertow vertical shear
 */
-# define MPI
+# undef  MPI
 # undef  NC4PAR
 # define SOLVE3D
 # define NEW_S_COORD
@@ -1455,7 +1450,6 @@
 # define WAVE_MAKER
 # define WAVE_MAKER_SPECTRUM
 # define WAVE_MAKER_DSPREAD
-# define WAVE_MAKER_OBLIQUE
 # define UV_HADV_WENO5
 # define UV_VADV_WENO5
 # define W_HADV_WENO5
