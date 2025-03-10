2efeb224dc081cc58db327e4eac990742a9aec67
LE GAC <solene.le.gac@ifremer.fr>
2025-03-10 08:57:56 +0100

    Revert formatting, only change on logfile

--- SCRIPTS/Plurimonths_scripts/run_croco_forecast.bash
+++ SCRIPTS/Plurimonths_scripts/run_croco_forecast.bash
@@ -3,7 +3,7 @@
 ################### USER'S DEFINED PARAMETERS ##########################
 #
 #=======================================================================
-#	Define environment variables
+#  Define environment variables
 #=======================================================================
 #
 # Environment for Crontab
@@ -12,14 +12,14 @@ OS=`uname`
 echo "OPERATING SYSTEM IS: $OS"
 
 if [ -f /etc/bashrc ]; then
-	. /etc/bashrc
+  . /etc/bashrc
 fi
 if [[ $OS == Linux ]] ; then
-	source ~/.bashrc
+  source ~/.bashrc
 elif [[ $OS == Darwin ]] ; then
-	source ~/.bash_profile
+  source ~/.bash_profile
 else
-	echo "unknown operating system" 1>&2
+  echo "unknown operating system" 1>&2
 fi
 #
 export TOOLSDIR=$HOME/croco/croco_tools/Forecast_tools
@@ -35,7 +35,7 @@ export LN=/bin/ln
 export DATESTR=`date +%Y-%m-%d`
 #
 #=======================================================================
-#	Define options
+#  Define options
 #=======================================================================
 #
 echo Start Forecast 
@@ -76,15 +76,15 @@ NDAYS_HIND=1
 # Forecast depth [days]
 NDAYS_FCST=3
 # Output frequency [hours]
-#	 average
+#   average
 ND_AVG=24
-#	 history (if = -1 set equal to NUMTIMES)
+#   history (if = -1 set equal to NUMTIMES)
 ND_HIS=6
-#	 restart (if = -1 set equal to NUMTIMES)
+#   restart (if = -1 set equal to NUMTIMES)
 ND_RST=24
 # 
 #=======================================================================
-#	Define directories, files and running parameters
+#  Define directories, files and running parameters
 #=======================================================================
 #
 export SCRATCHDIR=${RUNDIR}/SCRATCH
@@ -107,7 +107,7 @@ export BRYFILE=${MODEL}_bry_mercator_0.nc
 export CLMFILE=${MODEL}_clm_mercator_0.nc
 export INIFILE0=${MODEL}_ini_mercator_0.nc
 #
-################	END USER'S DEFINED PARAMETERS ##########################
+################  END USER'S DEFINED PARAMETERS ##########################
 #
 # Go the Input directory 
 # 
@@ -116,58 +116,58 @@ cd $INPUTDIR
 # Create working directories if needed
 #
 if [[ ! -e $SCRATCHDIR ]]; then
-	mkdir $SCRATCHDIR
+    mkdir $SCRATCHDIR
 else
-	echo "$SCRATCHDIR already exists" 1>&2
+    echo "$SCRATCHDIR already exists" 1>&2
 fi
 if [[ ! -e $MSSOUT ]]; then
-	mkdir $MSSOUT
+    mkdir $MSSOUT
 else
-	echo "$MSSOUT already exists" 1>&2
+    echo "$MSSOUT already exists" 1>&2
 fi
 #
 # Clean I/O files from previous forecast
 #
 if [ $CLEAN = 1 ] ; then
-	rm -f $MSSDIR/${MODEL}_blk_* $MSSDIR/${MODEL}_frc_* $MSSDIR/${MODEL}_bry_*
-	rm -f $MSSDIR/${MODEL}_clm_* $MSSDIR/${MODEL}_ini_*
-	rm -f $MSSOUT/${MODEL}_his_* $MSSOUT/${MODEL}_avg_* $MSSOUT/${MODEL}_rst_*
+  rm -f $MSSDIR/${MODEL}_blk_* $MSSDIR/${MODEL}_frc_* $MSSDIR/${MODEL}_bry_*
+  rm -f $MSSDIR/${MODEL}_clm_* $MSSDIR/${MODEL}_ini_*
+  rm -f $MSSOUT/${MODEL}_his_* $MSSOUT/${MODEL}_avg_* $MSSOUT/${MODEL}_rst_*
 fi
 #
 #=======================================================================
 # PRODUCE INITIAL CONDITIONS AND FORCING
 #=======================================================================
 # Compute lateral boundaries from MERCATOR
-#	 and surface forcing from GFS
+#     and surface forcing from GFS
 #
 if [ $PRE_PROCESS = 1 ]; then
-	echo "Processing boundary and forcing files"
-	[ ! -e start.m ] && { echo "==> Stop : you need a start.m file"; exit 1 ; }
-	$MATLAB -nodisplay -batch "run('start.m') ; run('make_forecast.m');" > matlab_forecast.out
+  echo "Processing boundary and forcing files"
+  [ ! -e start.m ] && { echo "==> Stop : you need a start.m file"; exit 1 ; }
+  $MATLAB -nodisplay -batch "run('start.m') ; run('make_forecast.m');" > matlab_forecast.out
 fi
 #
 # Copy files in SCRATCH dir
 #
 echo "Getting $BLKFILE from $MSSDIR"
-$CP -f	$MSSDIR/$BLKFILE $SCRATCHDIR
+$CP -f  $MSSDIR/$BLKFILE $SCRATCHDIR
 echo "Getting $FRCFILE from $MSSDIR"
-$CP -f	$MSSDIR/$FRCFILE $SCRATCHDIR
+$CP -f  $MSSDIR/$FRCFILE $SCRATCHDIR
 echo "Getting $BRYFILE from $MSSDIR"
-$CP -f	$MSSDIR/$BRYFILE $SCRATCHDIR
+$CP -f  $MSSDIR/$BRYFILE $SCRATCHDIR
 echo "Getting $CLMFILE from $MSSDIR"
-$CP -f	$MSSDIR/$CLMFILE $SCRATCHDIR
+$CP -f  $MSSDIR/$CLMFILE $SCRATCHDIR
 #
 # Initial file:
-#	if RESTART, get initial conditions 
-#	from the previous hindcast run
-#	else use global data.
+#  if RESTART, get initial conditions 
+#  from the previous hindcast run
+#  else use global data.
 #
 if [ $RESTART = 1 ] ; then
-	echo "Getting $INIFILE from $MSSOUT"
-	$CP -f	$MSSOUT/$INIFILE $SCRATCHDIR
+  echo "Getting $INIFILE from $MSSOUT"
+  $CP -f  $MSSOUT/$INIFILE $SCRATCHDIR
 else
-	echo "Getting $INIFILE from $MSSDIR"
-	$CP -f	$MSSDIR/$INIFILE0 $SCRATCHDIR/$INIFILE
+  echo "Getting $INIFILE from $MSSDIR"
+  $CP -f  $MSSDIR/$INIFILE0 $SCRATCHDIR/$INIFILE
 fi
 #
 # Get static files
@@ -186,56 +186,52 @@ $CP -f $INPUTDIR/${MODEL}_stations.in $SCRATCHDIR
 #
 NDAYS=$((NDAYS_HIND + NDAYS_FCST ))
 NUMTIMES=$((NDAYS * 24 * 3600 / DT))
-[[ ${ND_AVG} -ne -1 ]] &&	NUMAVG=$((ND_AVG * 3600 / DT )) || NUMAVG=$NUMTIMES
-[[ ${ND_HIS} -ne -1 ]] &&	NUMHIS=$((ND_HIS * 3600 / DT )) || NUMHIS=$NUMTIMES
-[[ ${ND_RST} -ne -1 ]] &&	NUMRST=$((ND_RST * 3600 / DT )) || NUMRST=$NUMTIMES
+[[ ${ND_AVG} -ne -1 ]] &&  NUMAVG=$((ND_AVG * 3600 / DT )) || NUMAVG=$NUMTIMES
+[[ ${ND_HIS} -ne -1 ]] &&  NUMHIS=$((ND_HIS * 3600 / DT )) || NUMHIS=$NUMTIMES
+[[ ${ND_RST} -ne -1 ]] &&  NUMRST=$((ND_RST * 3600 / DT )) || NUMRST=$NUMTIMES
 
-sed -e 's/NUMTIMES/'$NUMTIMES'/' \
-		-e 's/TIMESTEP/'$DT'/' \
-		-e 's/NFAST/'$NFAST'/' \
-		-e 's/NUMAVG/'$NUMAVG'/' \
-		-e 's/NUMHIS/'$NUMHIS'/' \
-		-e 's/NUMRST/'$NUMRST'/' \
-		-e 's/<logfilename>/'${MODEL}_${TIME}.out'/' \
-		< $INPUTDIR/${MODEL}_forecast.in > $SCRATCHDIR/${MODEL}_forecast.in
+sed -e 's/NUMTIMES/'$NUMTIMES'/' -e 's/TIMESTEP/'$DT'/' -e 's/NFAST/'$NFAST'/' \
+    -e 's/NUMAVG/'$NUMAVG'/' -e 's/NUMHIS/'$NUMHIS'/' -e 's/NUMRST/'$NUMRST'/' \
+    -e 's/<logfilename>/'${MODEL}_${TIME}.out'/' \
+        < $INPUTDIR/${MODEL}_forecast.in > $SCRATCHDIR/${MODEL}_forecast.in
 #
-#	Change directory
+#  Change directory
 #
 cd $SCRATCHDIR
 #
 #=======================================================================
-#	HINDCAST/FORECAST RUN
+#  HINDCAST/FORECAST RUN
 #=======================================================================
 #
 if [ $RUN = 1 ] ;then
-	echo Hindcast/Forecast run
-	date
-	$EXEC $CODFILE ${MODEL}_forecast.in > ${MODEL}_forecast_${DATESTR}.out
-	date
-	#
-	# Store the initial file for the next forecast
-	#
-	$CP -f $SCRATCHDIR/$RSTFILE ${MSSOUT}/$INIFILE
-	#
-	# Store the output forecast files
-	#
-	$CP $SCRATCHDIR/$HISFILE ${MSSOUT}/${MODEL}_his_forecast_${DATESTR}.nc
-	$CP $SCRATCHDIR/$AVGFILE ${MSSOUT}/${MODEL}_avg_forecast_${DATESTR}.nc
+  echo Hindcast/Forecast run
+  date
+  $EXEC $CODFILE ${MODEL}_forecast.in > ${MODEL}_forecast_${DATESTR}.out
+  date
+  #
+  # Store the initial file for the next forecast
+  #
+  $CP -f $SCRATCHDIR/$RSTFILE ${MSSOUT}/$INIFILE
+  #
+  # Store the output forecast files
+  #
+  $CP $SCRATCHDIR/$HISFILE ${MSSOUT}/${MODEL}_his_forecast_${DATESTR}.nc
+  $CP $SCRATCHDIR/$AVGFILE ${MSSOUT}/${MODEL}_avg_forecast_${DATESTR}.nc
 fi
 #
 #=======================================================================
-#	PLOT RESULTS
+#  PLOT RESULTS
 #=======================================================================
 #
 if [ $PLOT = 1 ] ;then
-	#
-	cd $INPUTDIR
-	[ ! -e start.m ] && { echo "==> Stop : you need a start.m file"; exit 1 ; }
-	# 
-	# Production plot
-	[ ! -e plot_forecast_croco.m ] && $CP -f ${TOOLSDIR}/plot_forecast_croco.m .
-	$MATLAB -nodisplay -batch	"run('plot_forecast_croco.m');" > plot_forecast_croco.out
-	#
+  #
+  cd $INPUTDIR
+  [ ! -e start.m ] && { echo "==> Stop : you need a start.m file"; exit 1 ; }
+  # 
+  # Production plot
+  [ ! -e plot_forecast_croco.m ] && $CP -f ${TOOLSDIR}/plot_forecast_croco.m .
+  $MATLAB -nodisplay -batch  "run('plot_forecast_croco.m');" > plot_forecast_croco.out
+  #
 fi
 
 echo Forecast finished
