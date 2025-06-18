9d9b35425b26ad6cb028f9729f12c2071e69bba8
LE GAC <solene.le.gac@ifremer.fr>
2025-06-18 08:52:01 +0200

    use Messaging to replace print

--- BENCH/libs/benchmarking.py
+++ BENCH/libs/benchmarking.py
@@ -236,4 +236,5 @@ class Benchmarking:
             run_shell_command(f"hwloc-ls --of console {results}/cpu.txt")
             run_shell_command(f"hwloc-ls --of svg {results}/cpu.svg")
         else:
-            print("Warning: 'hwloc-ls' not found. Skipping hardware topology capture.")
+            Messaging.step("Warning: 'hwloc-ls' not found.)")
+            Messaging.step(" Skipping hardware topology capture.")
