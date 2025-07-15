0dce11d634b2c52e115bf9915460e27606489ae0
SCHREIBER Martin <martin.schreiber@inria.fr>
2025-07-15 10:39:42 +0200

    cleanups step 1

--- OCEAN/compute_auxiliary_bounds.h
+++ OCEAN/compute_auxiliary_bounds.h
@@ -31,7 +31,13 @@
  bounds, it must be included just after the last type declaration
  inside a subroutine, but before the first executable statement.
 */
-      integer IstrR,IendR,JstrR,JendR
+      integer :: IstrR,IendR,JstrR,JendR
+
+#ifdef MPI_LAT_HID_2D
+      integer :: Istr_orig, Iend_orig, Jstr_orig, Jend_orig
+      integer :: lat_hid_2d_subtimestep
+#endif
+
 #ifdef EW_PERIODIC
 # define IstrU Istr
 #else
@@ -90,3 +96,69 @@
       else
         JendR=Jend
       endif
+
+#include "latency_hiding_2d.h"
+
+#ifdef MPI_LAT_HID_2D
+      Istr_orig = Istr
+      Iend_orig = Iend
+      Jstr_orig = Jstr
+      Jend_orig = Jend
+#else
+#  define Istr_orig Istr
+#  define Iend_orig Iend
+#  define Jstr_orig Jstr
+#  define Jend_orig Jend
+#endif
+
+#define IJ_ORIG Istr_orig,Iend_orig,Jstr_orig,Jend_orig
+
+
+#ifdef MPI_LAT_HID_2D_disabled
+
+# ifdef EW_PERIODIC
+
+      IstrR = IstrR - MPI_LAT_HID_2D_ADD_LAYERS
+      IendR = IendR + MPI_LAT_HID_2D_ADD_LAYERS
+
+      Istr = Istr - MPI_LAT_HID_2D_ADD_LAYERS
+      Iend = Iend + MPI_LAT_HID_2D_ADD_LAYERS
+
+# else
+
+      if (.not. (WESTERN_EDGE)) then
+        IstrR = IstrR - MPI_LAT_HID_2D_ADD_LAYERS
+        Istr = Istr - MPI_LAT_HID_2D_ADD_LAYERS
+      endif
+
+      if (.not. (EASTERN_EDGE)) then
+        IendR = IendR + MPI_LAT_HID_2D_ADD_LAYERS
+        Iend = Iend + MPI_LAT_HID_2D_ADD_LAYERS
+      endif
+
+# endif
+
+
+# ifdef NS_PERIODIC
+
+      JstrR = JstrR - MPI_LAT_HID_2D_ADD_LAYERS
+      JendR = JendR + MPI_LAT_HID_2D_ADD_LAYERS
+
+      Jstr = Jstr - MPI_LAT_HID_2D_ADD_LAYERS
+      Jend = Jend + MPI_LAT_HID_2D_ADD_LAYERS
+
+# else
+
+      if (.not. (SOUTHERN_EDGE)) then
+        JstrR = JstrR - MPI_LAT_HID_2D_ADD_LAYERS
+        Jstr = Jstr - MPI_LAT_HID_2D_ADD_LAYERS
+      endif
+
+      if (.not. (NORTHERN_EDGE)) then
+        JendR = JendR + MPI_LAT_HID_2D_ADD_LAYERS
+        Jend = Jend + MPI_LAT_HID_2D_ADD_LAYERS
+      endif
+
+# endif
+
+#endif
