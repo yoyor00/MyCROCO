b97778ae5ec859d0e737ad6f6367af886da474b8
Anne-Lou SCHAEFER <alschaef@datarmor1.ib0.ice.ifremer.fr>
2026-01-21 23:22:05 +0000

    first changes of MLD, to check if conflict after merging master

--- OCEAN/cppdefs.h
+++ OCEAN/cppdefs.h
@@ -327,7 +327,7 @@
 #  undef  DIAGNOSTICS_TS_ADV
 #  undef  DIAGNOSTICS_TS_MLD
 #  ifdef DIAGNOSTICS_TS_MLD
-#   define DIAGNOSTICS_TS_MLD_CRIT
+#   define DIAGNOSTICS_TS_MLD_CM
 #  endif
 
 # endif
