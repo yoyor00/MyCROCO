78d3aeea4af22fbccd3ad16035df8be5c04d27ad
Gildas Cambon <gildas.cambon@ird.fr>
2026-01-30 15:53:55 +0100

    - bug fix to manage coorectly exact_restart - clean the indentation using 2-space indentation instead of tabs

--- SCRIPTS/Plurimonths_scripts/run_croco.bash
+++ SCRIPTS/Plurimonths_scripts/run_croco.bash
@@ -91,8 +91,8 @@ LN=/bin/ln
 ########################################################
 #
 if [[ $TIME_SCHED == 0 ]]; then
-    NM_START=1
-    NM_END=1
+  NM_START=1
+  NM_END=1
 fi
 #
 # coarse netcdf file names
@@ -106,59 +106,45 @@ INIFILE=${MODEL}_ini.nc
 RNFFILE=${MODEL}_runoff.nc
 #
 #  Restart file - RSTFLAG=0 --> No Restart
-#		  RSTFLAG=1 --> Restart
+#                 RSTFLAG=1 --> Restart
 #
 if [[ $NY_START == 1 && $NM_START == 1 ]]; then
-    RSTFLAG=0
+  RSTFLAG=0
 else
-    RSTFLAG=1
+  RSTFLAG=1
 fi
-#  Exact restart 
-if [[ $EXACT_RST == 1 ]]; then
-    echo "Exact restart defined"
-    if [[ $NY == $NY_START && $NM == $NM_START ]]; then
-	NUMRECINI=1
-	echo "set NUMRECINI = $NUMRECINI"
-    else  # no exact restart
-	NUMRECINI=2
-	echo "set NUMRECINI = $NUMRECINI"
-    fi
-else
-    echo "No exact restart"
-    NUMRECINI=1
-    echo "set NUMRECINI = $NUMRECINI"
-fi
+
 
 #
 if [[ $RSTFLAG != 0 ]]; then
-    NY=$NY_START
-    NM=$NM_START
-    if [[ $TIME_SCHED == 0 ]]; then
-	NY=$((NY - 1))
-	TIME=Y${NY}
-    else
-	NM=$((NM - 1))
-	if [[ $NM == 0 ]]; then
-	    NM=12 
-	    NY=$((NY - 1))
-	fi
-	TIME=Y${NY}M$( printf ${MTH_FORMAT} ${NM})
+  NY=$NY_START
+  NM=$NM_START
+  if [[ $TIME_SCHED == 0 ]]; then
+    NY=$((NY - 1))
+    TIME=Y${NY}
+  else
+    NM=$((NM - 1))
+    if [[ $NM == 0 ]]; then
+      NM=12 
+      NY=$((NY - 1))
     fi
-    RSTFILE=${MODEL}_rst_${TIME}.nc
+    TIME=Y${NY}M$( printf ${MTH_FORMAT} ${NM})
+  fi
+  RSTFILE=${MODEL}_rst_${TIME}.nc
 fi
 #
 # Get the code
 #
 if [ ! -e $SCRATCHDIR ] ; then
-    mkdir $SCRATCHDIR
+  mkdir $SCRATCHDIR
 fi
 cd $SCRATCHDIR
 echo "Getting $CODFILE from $INPUTDIR"
 $CP -f $INPUTDIR/$CODFILE $SCRATCHDIR
 chmod u+x $CODFILE
 if [[ $NLEVEL -gt 1 ]]; then
-    echo "Getting $AGRIF_FILE from $INPUTDIR"
-    $CP -f $INPUTDIR/$AGRIF_FILE $SCRATCHDIR
+  echo "Getting $AGRIF_FILE from $INPUTDIR"
+  $CP -f $INPUTDIR/$AGRIF_FILE $SCRATCHDIR
 fi
 #
 # Get the netcdf files
@@ -167,33 +153,33 @@ LEVEL=0
 echo "Getting ${BRYFILE} from $MSSDIR"
 $LN -sf $MSSDIR/${BRYFILE} $SCRATCHDIR
 while [[ $LEVEL != $NLEVEL ]]; do
-    if [[ ${LEVEL} == 0 ]]; then
-	ENDF=
-    else
-	ENDF=.${LEVEL}
-    fi
-    echo "Getting ${GRDFILE}${ENDF} from $MSSDIR"
-    $LN -sf $MSSDIR/${GRDFILE}${ENDF} $SCRATCHDIR
-    echo "Getting ${FORFILE}${ENDF} from $MSSDIR"
-    $LN -sf $MSSDIR/${FORFILE}${ENDF} $SCRATCHDIR
-    echo "Getting ${BLKFILE}${ENDF} from $MSSDIR"
-    $LN -sf $MSSDIR/${BLKFILE}${ENDF} $SCRATCHDIR
-    echo "Getting ${CLMFILE}${ENDF} from $MSSDIR"
-    $LN -sf $MSSDIR/${CLMFILE}${ENDF} $SCRATCHDIR
-    echo "Getting ${RNFFILE}${ENDF} from $MSSDIR"
-    $LN -sf $MSSDIR/${RNFFILE}${ENDF} $SCRATCHDIR
-    if [[ $RSTFLAG == 0 ]]; then
-	echo "Getting ${INIFILE}${ENDF} from $MSSDIR"
-	$CP -f $MSSDIR/${INIFILE}${ENDF} $SCRATCHDIR
-    else
-	echo "Getting ${RSTFILE}${ENDF} from $MSSOUT"
-	$CP -f $MSSOUT/${RSTFILE}${ENDF} $SCRATCHDIR
-	$CP -f ${RSTFILE}${ENDF} ${MODEL}_ini.nc${ENDF}
-    fi
-    echo "Getting ${MODEL}_inter.in${ENDF} from $INPUTDIR"
-    $CP -f $INPUTDIR/${MODEL}_inter.in${ENDF} ${MODEL}_inter.in${ENDF}
+  if [[ ${LEVEL} == 0 ]]; then
+    ENDF=
+  else
+    ENDF=.${LEVEL}
+  fi
+  echo "Getting ${GRDFILE}${ENDF} from $MSSDIR"
+  $LN -sf $MSSDIR/${GRDFILE}${ENDF} $SCRATCHDIR
+  echo "Getting ${FORFILE}${ENDF} from $MSSDIR"
+  $LN -sf $MSSDIR/${FORFILE}${ENDF} $SCRATCHDIR
+  echo "Getting ${BLKFILE}${ENDF} from $MSSDIR"
+  $LN -sf $MSSDIR/${BLKFILE}${ENDF} $SCRATCHDIR
+  echo "Getting ${CLMFILE}${ENDF} from $MSSDIR"
+  $LN -sf $MSSDIR/${CLMFILE}${ENDF} $SCRATCHDIR
+  echo "Getting ${RNFFILE}${ENDF} from $MSSDIR"
+  $LN -sf $MSSDIR/${RNFFILE}${ENDF} $SCRATCHDIR
+  if [[ $RSTFLAG == 0 ]]; then
+    echo "Getting ${INIFILE}${ENDF} from $MSSDIR"
+    $CP -f $MSSDIR/${INIFILE}${ENDF} $SCRATCHDIR
+  else
+    echo "Getting ${RSTFILE}${ENDF} from $MSSOUT"
+    $CP -f $MSSOUT/${RSTFILE}${ENDF} $SCRATCHDIR
+    $CP -f ${RSTFILE}${ENDF} ${MODEL}_ini.nc${ENDF}
+  fi
+  echo "Getting ${MODEL}_inter.in${ENDF} from $INPUTDIR"
+  $CP -f $INPUTDIR/${MODEL}_inter.in${ENDF} ${MODEL}_inter.in${ENDF}
 
-    LEVEL=$((LEVEL + 1))
+  LEVEL=$((LEVEL + 1))
 done
 #
 # Put the number of time steps in the .in files
@@ -202,51 +188,51 @@ NUMTIMES=0
 NUMTIMES=$((NDAYS * 24 * 3600))
 NUMTIMES=$((NUMTIMES / DT))
 if [[ $TIME_SCHED == 0 ]]; then
-	NUMTIMES=$((NUMTIMES * 12))
+  NUMTIMES=$((NUMTIMES * 12))
 fi
 
 DT0=$DT
 LEVEL=0
 while [[ $LEVEL != $NLEVEL ]]; do
-    if [[ ${LEVEL} == 0 ]]; then
-	ENDF=''
-    else
-	ENDF=.${LEVEL}
-	NUMTIMES=$((AGRIF_REF * NUMTIMES))
-	DT=$((DT / AGRIF_REF))
-    fi
-    NUMAVG=$((ND_AVG * 86400 / DT ))
-    if [[ $ND_HIS -ne -1 ]]; then
-	NUMHIS=$((ND_HIS * 86400 / DT ))
-    else
-	NUMHIS=$NUMTIMES
-    fi
-    if [[ $ND_RST -ne -1 ]]; then
-	NUMRST=$((ND_RST * 86400 / DT ))
-    else
-	NUMRST=$NUMTIMES
-    fi
-    echo " "
-    echo "Writing in ${MODEL}_inter.in${ENDF}"
-    echo "USING DT       = $DT"
-    echo "USING NFAST    = $NFAST"
-    echo "USING NUMTIMES = $NUMTIMES"
-    echo "USING NUMAVG   = $NUMAVG"
-    echo "USING NUMHIS   = $NUMHIS"
-    echo "USING NUMRST   = $NUMRST"
-    
-    if [ ! -f ${MODEL}_inter.in${ENDF} ]; then
-	echo "=="
-	echo "=> ERROR : miss the ${MODEL}_inter.in${ENDF} file"
-	echo "=="
-	exit 1
-    fi
-    sed -e 's/NUMTIMES/'$NUMTIMES'/' -e 's/TIMESTEP/'$DT'/' -e 's/NFAST/'$NFAST'/' \
-	-e 's/\bNUMAVG\b/'$NUMAVG'/' \
-	-e 's/\bNUMHIS\b/'$NUMHIS'/' \
-	-e 's/\bNUMRST\b/'$NUMRST'/' < ${MODEL}_inter.in${ENDF} > ${MODEL}_inter.in${ENDF}.tmp1
-    
-    LEVEL=$((LEVEL + 1))
+  if [[ ${LEVEL} == 0 ]]; then
+    ENDF=''
+  else
+    ENDF=.${LEVEL}
+    NUMTIMES=$((AGRIF_REF * NUMTIMES))
+    DT=$((DT / AGRIF_REF))
+  fi
+  NUMAVG=$((ND_AVG * 86400 / DT ))
+  if [[ $ND_HIS -ne -1 ]]; then
+    NUMHIS=$((ND_HIS * 86400 / DT ))
+  else
+    NUMHIS=$NUMTIMES
+  fi
+  if [[ $ND_RST -ne -1 ]]; then
+    NUMRST=$((ND_RST * 86400 / DT ))
+  else
+    NUMRST=$NUMTIMES
+  fi
+  echo " "
+  echo "Writing in ${MODEL}_inter.in${ENDF}"
+  echo "USING DT       = $DT"
+  echo "USING NFAST    = $NFAST"
+  echo "USING NUMTIMES = $NUMTIMES"
+  echo "USING NUMAVG   = $NUMAVG"
+  echo "USING NUMHIS   = $NUMHIS"
+  echo "USING NUMRST   = $NUMRST"
+
+  if [ ! -f ${MODEL}_inter.in${ENDF} ]; then
+    echo "=="
+    echo "=> ERROR : miss the ${MODEL}_inter.in${ENDF} file"
+    echo "=="
+    exit 1
+  fi
+  sed -e 's/NUMTIMES/'$NUMTIMES'/' -e 's/TIMESTEP/'$DT'/' -e 's/NFAST/'$NFAST'/' \
+    -e 's/\bNUMAVG\b/'$NUMAVG'/' \
+    -e 's/\bNUMHIS\b/'$NUMHIS'/' \
+    -e 's/\bNUMRST\b/'$NUMRST'/' < ${MODEL}_inter.in${ENDF} > ${MODEL}_inter.in${ENDF}.tmp1
+
+  LEVEL=$((LEVEL + 1))
 done
 DT=$DT0
 #
@@ -258,102 +244,100 @@ NY_END=$((NY_END + 1))
 NM_END=$((NM_END + 1))
 NY=$NY_START
 while [[ $NY != $NY_END ]]; do
-    if [[ $NY == $NY_START ]]; then
-	NM=$NM_START
-    else 
-	NM=1
-    fi
-    MY_YEAR=$NY
-    MY_YEAR=$((MY_YEAR + 1))
-    if [[ $MY_YEAR == $NY_END ]]; then
-	MONTH_END=$NM_END
-    else 
-	MONTH_END=13
-    fi
+  if [[ $NY == $NY_START ]]; then
+    NM=$NM_START
+  else
+    NM=1
+  fi
+  MY_YEAR=$NY
+  MY_YEAR=$((MY_YEAR + 1))
+  if [[ $MY_YEAR == $NY_END ]]; then
+    MONTH_END=$NM_END
+  else
+    MONTH_END=13
+  fi
+  if [[ $TIME_SCHED == 0 ]]; then
+    MONTH_END=2
+  fi
+  while [[ $NM != $MONTH_END ]]; do
     if [[ $TIME_SCHED == 0 ]]; then
-	MONTH_END=2
+      TIME=Y${NY}
+    else
+      TIME=Y${NY}M$( printf ${MTH_FORMAT} ${NM})
     fi
-    while [[ $NM != $MONTH_END ]]; do
-	if [[ $TIME_SCHED == 0 ]]; then
-	    TIME=Y${NY}
-	else
-	    TIME=Y${NY}M$( printf ${MTH_FORMAT} ${NM})
-	fi
-	
-	#
-	LEVEL=0
-	while [[ $LEVEL != $NLEVEL ]]; do
-	    if [[ ${LEVEL} == 0 ]]; then
-		ENDF=''
-	    else
-		ENDF=.${LEVEL}
-	    fi
-	if [[ $EXACT_RST == 1 ]]; then
-	    echo "Exact restart defined"
-	    if [[ $NY == $NY_START && $NM == $NM_START ]]; then
-		NUMRECINI=1
-		echo "set NUMRECINI = $NUMRECINI"
-	    else  # no exact restart
-		NUMRECINI=2
-		echo "set NUMRECINI = $NUMRECINI"
-	    fi
-	else
-	    echo "No exact restart"
-	    NUMRECINI=1
-	    echo "set NUMRECINI = $NUMRECINI"
-	fi
-	#
-	sed -e 's/NUMRECINI/'$NUMRECINI'/' \
-	    -e 's/<logfilename>/'${MODEL}_${TIME}.out'/' \
-		< ${MODEL}_inter.in${ENDF}.tmp1 > ${MODEL}.in${ENDF}
-	#
-	    LEVEL=$((LEVEL + 1))
-	done
-	#
-	#  COMPUTE
-	#
-	echo " "
-	echo "Computing for $TIME"
-	date
-	${RUNCMD}$CODFILE  ${MODEL}.in > ${MODEL}_${TIME}.out
-	date
-	#
-	# Test if the month has finised properly
-	echo "Test ${MODEL}_${TIME}.out"
-	status=`tail -2 ${MODEL}_${TIME}.out | grep DONE | wc -l`
-	if [[ $status -eq 1 ]]; then
-	    echo "All good"
-	    echo "XXXX${MYTEST}XXXX"
-	else
-	    echo
-	    echo "Warning: month not finished properly"
-	    echo
-	    tail -20 ${MODEL}_${TIME}.out
-	    echo
-	    echo "Month ${TIME} did not work"
-	    echo
-	    exit 1
-	fi
-	#
-	#  Archive
-	#
-	LEVEL=0
-	while [[ $LEVEL != $NLEVEL ]]; do
-	    if [[ ${LEVEL} == 0 ]]; then
-		ENDF=
-	    else
-		ENDF=.${LEVEL}
-	    fi
-	    $CP -f ${MODEL}_rst.nc${ENDF} ${INIFILE}${ENDF}
-	    $MV -f ${MODEL}_his.nc${ENDF} ${MSSOUT}/${MODEL}_his_${TIME}.nc${ENDF}
-	    $MV -f ${MODEL}_rst.nc${ENDF} ${MSSOUT}/${MODEL}_rst_${TIME}.nc${ENDF}
-	    $MV -f ${MODEL}_avg.nc${ENDF} ${MSSOUT}/${MODEL}_avg_${TIME}.nc${ENDF}
 
-	    LEVEL=$((LEVEL + 1))
-	done
-	NM=$((NM + 1))
+    #
+    LEVEL=0
+    while [[ $LEVEL != $NLEVEL ]]; do
+      if [[ ${LEVEL} == 0 ]]; then
+        ENDF=''
+      else
+        ENDF=.${LEVEL}
+      fi
+      if [[ $NY == 1 && $NM == 1 ]]; then
+        NUMRECINI=1
+        echo "set NUMRECINI = $NUMRECINI"
+      else
+        if [[ $EXACT_RST == 1 ]]; then
+          NUMRECINI=2
+          echo "set NUMRECINI = $NUMRECINI"
+        else
+          NUMRECINI=1
+          echo "set NUMRECINI = $NUMRECINI"
+        fi
+        #
+        sed -e 's/NUMRECINI/'$NUMRECINI'/' \
+          -e 's/<logfilename>/'${MODEL}_${TIME}.out'/' \
+          < ${MODEL}_inter.in${ENDF}.tmp1 > ${MODEL}.in${ENDF}
+        #
+      fi
+      LEVEL=$((LEVEL + 1))
     done
-    NY=$((NY + 1))
+    #
+    #  COMPUTE
+    #
+    echo " "
+    echo "Computing for $TIME"
+    date
+    ${RUNCMD}$CODFILE  ${MODEL}.in > ${MODEL}_${TIME}.out
+    date
+    #
+    # Test if the month has finised properly
+    echo "Test ${MODEL}_${TIME}.out"
+    status=`tail -2 ${MODEL}_${TIME}.out | grep DONE | wc -l`
+    if [[ $status -eq 1 ]]; then
+      echo "All good"
+      echo "XXXX${MYTEST}XXXX"
+    else
+      echo
+      echo "Warning: month not finished properly"
+      echo
+      tail -20 ${MODEL}_${TIME}.out
+      echo
+      echo "Month ${TIME} did not work"
+      echo
+      exit 1
+    fi
+    #
+    #  Archive
+    #
+    LEVEL=0
+    while [[ $LEVEL != $NLEVEL ]]; do
+      if [[ ${LEVEL} == 0 ]]; then
+        ENDF=
+      else
+        ENDF=.${LEVEL}
+      fi
+      $CP -f ${MODEL}_rst.nc${ENDF} ${INIFILE}${ENDF}
+      $MV -f ${MODEL}_his.nc${ENDF} ${MSSOUT}/${MODEL}_his_${TIME}.nc${ENDF}
+      $MV -f ${MODEL}_rst.nc${ENDF} ${MSSOUT}/${MODEL}_rst_${TIME}.nc${ENDF}
+      $MV -f ${MODEL}_avg.nc${ENDF} ${MSSOUT}/${MODEL}_avg_${TIME}.nc${ENDF}
+
+      LEVEL=$((LEVEL + 1))
+    done
+    NM=$((NM + 1))
+  done
+  NY=$((NY + 1))
 done
 #
 #############################################################
--- SCRIPTS/Plurimonths_scripts/run_croco_inter.bash
+++ SCRIPTS/Plurimonths_scripts/run_croco_inter.bash
@@ -104,7 +104,7 @@ ND_RST=-1
 
 # Output frequency [hours] - case 2 : USE_CALENDAR - USED ONLY IF USE_CALENDAR
 #
-USE_CALENDAR=1
+USE_CALENDAR=0
 #
 #  average (in hours)
 NHAVG_UC=$((24))
@@ -113,9 +113,14 @@ NHHIS_UC=-1
 #  restart (in hours, if = -1 set equal to NUMTIMES*DT/3600, the end of each month/year)
 NHRST_UC=-1
 
-#  Restart file - RSTFLAG=0 --> No Restart
-#		  RSTFLAG=1 --> Restart
-RSTFLAG=0     
+#  Restart file - RSTFLAG=0 --> No Restart  FOR BEGINING OF SIMULATION
+#		              RSTFLAG=1 --> Restart
+RSTFLAG=1
+
+# Cold start Year and Month - define the Year and Month of the cold start ini file
+NY_COLDSTART=2005
+NM_COLDSTART=1
+
 #  Exact restart - EXACT_RST=0 --> Exact restart OFF
 #                - EXACT_RST=1 --> Exact restart ON
 EXACT_RST=0
@@ -148,7 +153,7 @@ RNFFILE=${MODEL}_runoff
 TIDEFILE=${MODEL}_frc
 #
 if [ ! -e $MSSOUT ] ; then
- mkdir $MSSOUT
+  mkdir $MSSOUT
 fi
 #
 if [[ $RSTFLAG != 0 ]]; then
@@ -177,7 +182,7 @@ fi
 # Get the code
 #
 if [ ! -e $SCRATCHDIR ] ; then
- mkdir $SCRATCHDIR
+  mkdir $SCRATCHDIR
 fi
 cd $SCRATCHDIR
 echo "Getting $CODFILE from $INPUTDIR"
@@ -222,30 +227,30 @@ NY=$NY_START
 while [ $NY != $NY_END ]; do
   if [[ $NY == $NY_START ]]; then
     NM=$NM_START
-  else 
-     NM=1
+  else
+    NM=1
   fi
-   MY_YEAR=$NY
-   MY_YEAR=$((MY_YEAR + 1))
+  MY_YEAR=$NY
+  MY_YEAR=$((MY_YEAR + 1))
   if [[ $MY_YEAR == $NY_END ]]; then
-     MONTH_END=$NM_END
-  else 
-     MONTH_END=13
+    MONTH_END=$NM_END
+  else
+    MONTH_END=13
   fi
   if [[ $TIME_SCHED == 0 ]]; then
-     MONTH_END=2
+    MONTH_END=2
   fi
   while [ $NM != $MONTH_END ]; do
     if [[ $TIME_SCHED == 0 ]]; then
       TIME=Y${NY}
       echo "Computing YEAR $NY"
     else
-	TIME=Y${NY}M$( printf ${MTH_FORMAT} ${NM})
-	echo "Computing YEAR $NY MONTH $( printf ${MTH_FORMAT} ${NM})"
+      TIME=Y${NY}M$( printf ${MTH_FORMAT} ${NM})
+      echo "Computing YEAR $NY MONTH $( printf ${MTH_FORMAT} ${NM})"
     fi
-#
-# Get forcing and clim for this time
-#
+    #
+    # Get forcing and clim for this time
+    #
     LEVEL=0
     while [ $LEVEL != $NLEVEL ]; do
       if [[ ${LEVEL} == 0 ]]; then
@@ -258,23 +263,23 @@ while [ $NY != $NY_END ]; do
         $LN -sf $MSSDIR/${FRCFILE}_${ATMOS_FRC}_${TIME}.nc${ENDF} ${FRCFILE}.nc${ENDF}
       fi
       if [[ ${BULK_FILES} == 1 ]]; then
-          echo "Getting ${BLKFILE}_${ATMOS_BULK}_${TIME}.nc${ENDF} from $MSSDIR"
-          $LN -sf $MSSDIR/${BLKFILE}_${ATMOS_BULK}_${TIME}.nc${ENDF} ${BLKFILE}.nc${ENDF}
+        echo "Getting ${BLKFILE}_${ATMOS_BULK}_${TIME}.nc${ENDF} from $MSSDIR"
+        $LN -sf $MSSDIR/${BLKFILE}_${ATMOS_BULK}_${TIME}.nc${ENDF} ${BLKFILE}.nc${ENDF}
       fi
       if [[ ${RUNOFF_FILES} == 1 ]]; then
-          echo "Getting ${RNFFILE}.nc${ENDF} from $MSSDIR"
-          $LN -sf $MSSDIR/${RNFFILE}.nc${ENDF} ${RNFFILE}.nc${ENDF}
+        echo "Getting ${RNFFILE}.nc${ENDF} from $MSSDIR"
+        $LN -sf $MSSDIR/${RNFFILE}.nc${ENDF} ${RNFFILE}.nc${ENDF}
       fi
       if [[ ${TIDE_FILES} == 1 ]]; then
-          echo "Getting ${TIDEFILE}_${TIDE_FRC}.nc${ENDF} from $MSSDIR"
-	  $LN -sf $MSSDIR/${TIDEFILE}_${TIDE_FRC}.nc${ENDF} ${TIDEFILE}.nc${ENDF}
+        echo "Getting ${TIDEFILE}_${TIDE_FRC}.nc${ENDF} from $MSSDIR"
+        $LN -sf $MSSDIR/${TIDEFILE}_${TIDE_FRC}.nc${ENDF} ${TIDEFILE}.nc${ENDF}
       fi
 
       LEVEL=$((LEVEL + 1))
     done
-#
-# No child climatology or boundary files
-#
+    #
+    # No child climatology or boundary files
+    #
     if [[ ${CLIMATOLOGY_FILES} == 1 ]]; then
       echo "Getting ${CLMFILE}_${OGCM}_${TIME}.nc from $MSSDIR"
       $LN -sf $MSSDIR/${CLMFILE}_${OGCM}_${TIME}.nc ${CLMFILE}.nc
@@ -283,19 +288,19 @@ while [ $NY != $NY_END ]; do
       echo "Getting ${BRYFILE}_${OGCM}_${TIME}.nc from $MSSDIR"
       $LN -sf $MSSDIR/${BRYFILE}_${OGCM}_${TIME}.nc ${BRYFILE}.nc
     fi
-#
-# Set the number of time steps for each month 
-# (30 or 31 days + 28 or 29 days for february)
-#
+    #
+    # Set the number of time steps for each month
+    # (30 or 31 days + 28 or 29 days for february)
+    #
     NUMTIMES=0
-#
+    #
     if [[ ${NM} == 1 || ${NM} == 3 || ${NM} == 5 || ${NM} == 7 || ${NM} == 8 || ${NM} == 10 || ${NM} == 12 ]]; then
       NDAYS=31
     else
       NDAYS=30
       if [[ ${NM} == 2 ]]; then
         NDAYS=28
-# February... check if it is a leap year
+        # February... check if it is a leap year
 
         B4=0
         B100=0
@@ -305,23 +310,23 @@ while [ $NY != $NY_END ]; do
         B100=$((100 * ( NY / 100 )))
         B400=$((400 * ( NY / 400 )))
 
-	
+
         if [[ $NY == $B4 && ((!($NY == $B100))||($NY == $B400)) ]]; then
-#
+          #
           BSPIN=$(( NY - NY_START + 1 ))
           if [[ $BSPIN -gt $NY_SPIN ]]; then
-	     echo Leap Year - $NY $B4 $B100 $B400
-             NDAYS=29
+            echo Leap Year - $NY $B4 $B100 $B400
+            NDAYS=29
           else
-#.........   SPINUP!!!! In case of spinup I cant have leap years.
-	     echo year $NY should be a Leap Year     
-	     echo 'BUT : Spinup case: no leap year'
-             NDAYS=28
+            #.........   SPINUP!!!! In case of spinup I cant have leap years.
+            echo year $NY should be a Leap Year
+            echo 'BUT : Spinup case: no leap year'
+            NDAYS=28
           fi
-#
+          #
         else
-	  echo Not a Leap Year - $NY $B4 $B100 $B400
-          NDAYS=28	  		  
+          echo Not a Leap Year - $NY $B4 $B100 $B400
+          NDAYS=28
         fi
       fi
     fi
@@ -335,119 +340,119 @@ while [ $NY != $NY_END ]; do
     DT0=$DT
     LEVEL=0
     while [[ $LEVEL != $NLEVEL ]]; do
-	if [[ ${LEVEL} == 0 ]]; then
-            ENDF=
-	else
-            ENDF=.${LEVEL}
-	    NUMTIMES=$((AGRIF_REF * NUMTIMES))
-	    DT=$((DT / AGRIF_REF))
-	fi
-	NUMAVG=$((ND_AVG * 86400 / DT ))
-	if [[ ${ND_HIS} -ne -1 ]]; then
-	    NUMHIS=$((ND_HIS * 86400 / DT ))
-	else
-	    NUMHIS=$NUMTIMES
-	fi
-	if [[ ${ND_RST} -ne -1 ]]; then
-	    NUMRST=$((ND_RST * 86400 / DT ))
-	else
-	    NUMRST=$NUMTIMES
-	fi
-	if [[ $USE_CALENDAR == 1 ]]; then
-	    echo "USE_CALENDAR defined"
-	    NHAVG=$((NHAVG_UC))
-	    if [[ ${NHHIS_UC} -ne -1 ]]; then
-		NHHIS=$((NHHIS_UC))
-	    else
-		NHHIS=$((NDAYS * 24))
-	    fi
-	    if [[ ${NHRST_UC} -ne -1 ]]; then
-		NHRST=$((NHRST_UC))
-	    else
-		NHRST=$((NDAYS * 24))
-	    fi
-	fi
-	if [[ $EXACT_RST == 1 ]]; then
-	    echo "Exact restart defined"
-	    if [[ $NY == $NY_START && $NM == $NM_START ]]; then
-		NUMRECINI=1
-		echo "set NUMRECINI = $NUMRECINI"
-	    else
-		NUMRECINI=2
-		echo "set NUMRECINI = $NUMRECINI"
-	    fi
-	else  # no exact restart
-	    echo "No exact restart"
-	    NUMRECINI=1
-	    echo "set NUMRECINI = $NUMRECINI"
-	fi
-	
-	echo " "
-	echo "Writing in ${MODEL}_inter.in${ENDF}"
-	echo "USING DT       = $DT"
-	echo "USING NFAST    = $NFAST"
-	echo "USING NUMTIMES = $NUMTIMES"
-	echo "USING NUMAVG   = $NUMAVG"
-	echo "USING NUMHIS   = $NUMHIS"
-	echo "USING NUMRST   = $NUMRST"
-	echo "USING NUMRECINI = $NUMRECINI"
-	echo "USING NYONLINE = $NY"
-	echo "USING NMONLINE = $NM"
-	echo "USING ENDYONLINE = $NY_END"
-	echo "USING ENDMONLINE = $NM_END"
-	echo "USING ONLINEFREQ = $ONLINEFREQ"
-	echo "USING ONLINEPATH = $ONLINEPATH"
-	
-	if [ ! -f ${MODEL}_inter.in${ENDF} ]; then
-	    echo "=="
-	    echo "=> ERROR : miss the ${MODEL}_inter.in${ENDF} file"
-	  echo "=="
-	  exit 1
-	fi
-        sed \
-          -e "s/NUMTIMES/${NUMTIMES}/" \
-          -e "s/TIMESTEP/${DT}/" \
-          -e "s/NFAST/${NFAST}/" \
-          -e "s/\bNUMAVG\b/${NUMAVG}/" \
-          -e "s/\bNUMHIS\b/${NUMHIS}/" \
-          -e "s/\bNUMRST\b/${NUMRST}/" \
-          -e "s/NUMRECINI/${NUMRECINI}/" \
-          -e "s/NYONLINE/${NY}/" \
-          -e "s/NMONLINE/${NM}/" \
-          -e "s/ENDYONLINE/${NY_END}/" \
-          -e "s/ENDMONLINE/${NM_END}/" \
-          -e "s/ONLINEFREQ/${ONLINEFREQ}/" \
-          -e "s|ONLINEPATH|${ONLINEPATH}|" \
-          -e "s|<logfilename>|${MODEL}_${TIME}.out|" \
-          < "${MODEL}_inter.in${ENDF}" > "${MODEL}_${TIME}_inter.in${ENDF}"
+      if [[ ${LEVEL} == 0 ]]; then
+        ENDF=
+      else
+        ENDF=.${LEVEL}
+        NUMTIMES=$((AGRIF_REF * NUMTIMES))
+        DT=$((DT / AGRIF_REF))
+      fi
+      NUMAVG=$((ND_AVG * 86400 / DT ))
+      if [[ ${ND_HIS} -ne -1 ]]; then
+        NUMHIS=$((ND_HIS * 86400 / DT ))
+      else
+        NUMHIS=$NUMTIMES
+      fi
+      if [[ ${ND_RST} -ne -1 ]]; then
+        NUMRST=$((ND_RST * 86400 / DT ))
+      else
+        NUMRST=$NUMTIMES
+      fi
+      if [[ $USE_CALENDAR == 1 ]]; then
+        echo "USE_CALENDAR defined"
+        NHAVG=$((NHAVG_UC))
+        if [[ ${NHHIS_UC} -ne -1 ]]; then
+          NHHIS=$((NHHIS_UC))
+        else
+          NHHIS=$((NDAYS * 24))
+        fi
+        if [[ ${NHRST_UC} -ne -1 ]]; then
+          NHRST=$((NHRST_UC))
+        else
+          NHRST=$((NDAYS * 24))
+        fi
+      fi
+      if [[ $EXACT_RST == 1 ]]; then
+        echo "Exact restart defined"
+        if [[ $NY == $NY_COLDSTART && $NM == $NM_COLDSTART ]]; then
+          NUMRECINI=1
+          echo "set NUMRECINI = $NUMRECINI"
+        else
+          NUMRECINI=2
+          echo "set NUMRECINI = $NUMRECINI"
+        fi
+      else  # no exact restart
+        echo "No exact restart"
+        NUMRECINI=1
+        echo "set NUMRECINI = $NUMRECINI"
+      fi
 
-	if [[ $USE_CALENDAR == 1 ]]; then
-	    if [[ ${NM} == 12 ]]; then
-		NM_E_UC=1
-		NY_E_UC=$((NY + 1))
-	    else
-		NM_E_UC=$((NM + 1))
-		NY_E_UC=$NY
-	    fi
-	    echo "USING Ystart   = $NY"
-	    echo "USING Mstart   = $(printf "%02d" $NM)"
-	    echo "USING Yend     = $NY_E_UC"
-	    echo "USING Mend     = $(printf "%02d" $NM_E_UC)"
-	    echo "USING NHHIS    = $NHHIS"
-	    echo "USING NHAVG    = $NHAVG"
-	    echo "USING NHRST    = $NHRST"
-	    sed -e "s/NHHIS/${NHHIS}/" \
-		-e "s/NHAVG/${NHAVG}/" \
-		-e "s/NHRST/${NHRST}/" \
-	    	-e "s/Ystart/${NY}/"   \
-		-e "s/Mstart/$(printf "%02d" $NM)/" \
-		-e "s/Yend/${NY_E_UC}/" \
-		-e "s/Mend/$(printf "%02d" $NM_E_UC)/" \
-		< "${MODEL}_${TIME}_inter.in${ENDF}" > "${MODEL}_${TIME}_inter_UC.in${ENDF}"
-	    mv "${MODEL}_${TIME}_inter_UC.in${ENDF}" "${MODEL}_${TIME}_inter.in${ENDF}"
-	    fi
-	#
-	LEVEL=$((LEVEL + 1))
+      echo " "
+      echo "Writing in ${MODEL}_inter.in${ENDF}"
+      echo "USING DT       = $DT"
+      echo "USING NFAST    = $NFAST"
+      echo "USING NUMTIMES = $NUMTIMES"
+      echo "USING NUMAVG   = $NUMAVG"
+      echo "USING NUMHIS   = $NUMHIS"
+      echo "USING NUMRST   = $NUMRST"
+      echo "USING NUMRECINI = $NUMRECINI"
+      echo "USING NYONLINE = $NY"
+      echo "USING NMONLINE = $NM"
+      echo "USING ENDYONLINE = $NY_END"
+      echo "USING ENDMONLINE = $NM_END"
+      echo "USING ONLINEFREQ = $ONLINEFREQ"
+      echo "USING ONLINEPATH = $ONLINEPATH"
+
+      if [ ! -f ${MODEL}_inter.in${ENDF} ]; then
+        echo "=="
+        echo "=> ERROR : miss the ${MODEL}_inter.in${ENDF} file"
+        echo "=="
+        exit 1
+      fi
+      sed \
+        -e "s/NUMTIMES/${NUMTIMES}/" \
+        -e "s/TIMESTEP/${DT}/" \
+        -e "s/NFAST/${NFAST}/" \
+        -e "s/\bNUMAVG\b/${NUMAVG}/" \
+        -e "s/\bNUMHIS\b/${NUMHIS}/" \
+        -e "s/\bNUMRST\b/${NUMRST}/" \
+        -e "s/NUMRECINI/${NUMRECINI}/" \
+        -e "s/NYONLINE/${NY}/" \
+        -e "s/NMONLINE/${NM}/" \
+        -e "s/ENDYONLINE/${NY_END}/" \
+        -e "s/ENDMONLINE/${NM_END}/" \
+        -e "s/ONLINEFREQ/${ONLINEFREQ}/" \
+        -e "s|ONLINEPATH|${ONLINEPATH}|" \
+        -e "s|<logfilename>|${MODEL}_${TIME}.out|" \
+        < "${MODEL}_inter.in${ENDF}" > "${MODEL}_${TIME}_inter.in${ENDF}"
+
+      if [[ $USE_CALENDAR == 1 ]]; then
+        if [[ ${NM} == 12 ]]; then
+          NM_E_UC=1
+          NY_E_UC=$((NY + 1))
+        else
+          NM_E_UC=$((NM + 1))
+          NY_E_UC=$NY
+        fi
+        echo "USING Ystart   = $NY"
+        echo "USING Mstart   = $(printf "%02d" $NM)"
+        echo "USING Yend     = $NY_E_UC"
+        echo "USING Mend     = $(printf "%02d" $NM_E_UC)"
+        echo "USING NHHIS    = $NHHIS"
+        echo "USING NHAVG    = $NHAVG"
+        echo "USING NHRST    = $NHRST"
+        sed -e "s/NHHIS/${NHHIS}/" \
+          -e "s/NHAVG/${NHAVG}/" \
+          -e "s/NHRST/${NHRST}/" \
+          -e "s/Ystart/${NY}/"   \
+          -e "s/Mstart/$(printf "%02d" $NM)/" \
+          -e "s/Yend/${NY_E_UC}/" \
+          -e "s/Mend/$(printf "%02d" $NM_E_UC)/" \
+          < "${MODEL}_${TIME}_inter.in${ENDF}" > "${MODEL}_${TIME}_inter_UC.in${ENDF}"
+        mv "${MODEL}_${TIME}_inter_UC.in${ENDF}" "${MODEL}_${TIME}_inter.in${ENDF}"
+      fi
+      #
+      LEVEL=$((LEVEL + 1))
     done
     DT=$DT0
     #
@@ -459,7 +464,7 @@ while [ $NY != $NY_END ]; do
     ${RUNCMD}$CODFILE  ${MODEL}_${TIME}_inter.in > ${MODEL}_${TIME}.out
     date
     #
-    
+
     # Test if the month has finised properly
     echo "Test ${MODEL}_${TIME}.out"
     status=`tail -2 ${MODEL}_${TIME}.out | grep DONE | wc -l`
@@ -476,9 +481,9 @@ while [ $NY != $NY_END ]; do
       echo
       exit 1
     fi
-#
-#  Archive
-#
+    #
+    #  Archive
+    #
     LEVEL=0
     while [[ $LEVEL != $NLEVEL ]]; do
       if [[ ${LEVEL} == 0 ]]; then
@@ -486,10 +491,10 @@ while [ $NY != $NY_END ]; do
       else
         ENDF=.${LEVEL}
       fi
-	  $CP -f ${MODEL}_rst.nc${ENDF} ${INIFILE}.nc${ENDF}
-	  $MV -f ${MODEL}_his.nc${ENDF} ${MSSOUT}/${MODEL}_his_${TIME}.nc${ENDF}
-	  $MV -f ${MODEL}_rst.nc${ENDF} ${MSSOUT}/${MODEL}_rst_${TIME}.nc${ENDF}
-	  $MV -f ${MODEL}_avg.nc${ENDF} ${MSSOUT}/${MODEL}_avg_${TIME}.nc${ENDF}
+      $CP -f ${MODEL}_rst.nc${ENDF} ${INIFILE}.nc${ENDF}
+      $MV -f ${MODEL}_his.nc${ENDF} ${MSSOUT}/${MODEL}_his_${TIME}.nc${ENDF}
+      $MV -f ${MODEL}_rst.nc${ENDF} ${MSSOUT}/${MODEL}_rst_${TIME}.nc${ENDF}
+      $MV -f ${MODEL}_avg.nc${ENDF} ${MSSOUT}/${MODEL}_avg_${TIME}.nc${ENDF}
       LEVEL=$((LEVEL + 1))
     done
     NM=$((NM + 1))
@@ -498,7 +503,6 @@ while [ $NY != $NY_END ]; do
 done
 #
 #############################################################
-
 
 
 
