e6d170c676e6535e792e863414d782b2cd9ad6bc
Martin Huret <martin.huret@ifremer.fr>
2026-07-28 11:19:18 +0200

    Commentaire supprimé : temperture mis à 0

--- FOIL/ibmtools.F90
+++ FOIL/ibmtools.F90
@@ -222,7 +222,6 @@ CONTAINS
                              Istr, Iend, Jstr, Jend)
       dens_surf = w_dens(temp_surf, sal_surf)
 
-    ! Si temperature realiste a l'initialisation, on prend sa valeur, sinon on prend 0
     particle%temp    = temp_surf
     particle%density = dens_surf
 
